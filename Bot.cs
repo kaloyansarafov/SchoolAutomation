5043563ca2a9010b8cbde1ded1418b6a8165d5f7
Lokixa <lyuben.kodinov@gmail.com>
2021-01-13 18:55:58 +0200

    chrome support

--- Bot.cs
+++ Bot.cs
@@ -10,76 +10,30 @@ using System;
 using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Firefox;
 using System.Diagnostics;
+using GoogleCRBot.Data;
 
 namespace GoogleCRBot
 {
     public class ClassroomBot : IDisposable
     {
-        Data data { get; init; }
-        IWebDriver driver;
-        StreamWriter logger;
-        WebDriverWait wait;
+        BrowserData browserData { get; }
+        GlobalData globalData { get; }
+        IWebDriver driver { get; }
+        WebDriverWait wait { get; }
         public ClassroomBot()
         {
-            data = JsonConvert.DeserializeObject<Data>(File.ReadAllText("data.json"));
-            logger = new StreamWriter(data.LogFolder + "/" + DateTime.Now.ToString("d-M-yyyy_h-mm-ss") + ".txt");
-            InitDriver();
-            wait = new WebDriverWait(driver, new TimeSpan(0, 0, 10));
-        }
-        void InitDriver()
-        {
-            try
-            {
-                Console.WriteLine("Attaching webdriver");
-                driver = new RemoteWebDriver(new Uri(data.RemoteURI), new FirefoxOptions());
-            }
-            catch (OpenQA.Selenium.WebDriverException)
-            {
-                Console.WriteLine("Starting webdriver");
-                StartWebDriver();
-
-                // Try to reatach
-                driver = new RemoteWebDriver(new Uri(data.RemoteURI), new FirefoxOptions());
-            }
-            // catch (InvalidOperationException)
-            // {
-            // Could restart web driver
-            // }
-            Console.WriteLine("Attached webdriver");
-        }
-        private void StartWebDriver()
-        {
-            string filename = data.DriverFolder + "/geckodriver.exe";
-            if (!File.Exists(filename))
-            {
-                throw new FileNotFoundException("Download 'geckodriver' and put it in the drivers folder");
-            }
-            Process proc = new Process();
-            proc.StartInfo.FileName = filename;
-            proc.StartInfo.RedirectStandardError = true;
-
-            proc.ErrorDataReceived += WriteLogs;
-
-            if (!proc.Start())
-            {
-                throw new OperationCanceledException("Couldn't start webdriver");
-            }
-        }
-
-        private void WriteLogs(object sender, DataReceivedEventArgs e)
-        {
-            var proc = sender as Process;
-            logger.WriteLine($"\nError: {proc.StandardError.ReadToEnd()}\n");
-            logger.Flush();
+            globalData = JsonConvert.DeserializeObject<GlobalData>(File.ReadAllText("data.json"));
+            driver = BrowserFactory.InitDriver(globalData.DriverFolder, globalData.PreferredBrowser);
+            browserData = BrowserFactory.GetBrowserData(globalData);
+            wait = new WebDriverWait(driver, new TimeSpan(0, 0, 5));
         }
 
         public void SendOnFirst(string message, string url)
         {
-            logger.WriteLine(url);
             wait.Until(driver => driver.Navigate()).GoToUrl(url);
             IWebElement el = wait.Until(driver => driver.FindElement(By.XPath("//*[@id=\":1.t\"]")));
             el.SendKeys(message);
-            el.SendKeys(Keys.Tab + Keys.Enter);
+            // el.SendKeys(Keys.Tab + Keys.Enter);
         }
         public bool Login()
         {
@@ -90,7 +44,7 @@ namespace GoogleCRBot
             }
             catch (Exception ex)
             {
-                logger.WriteLine("Got error: " + ex.Message);
+                Console.WriteLine("Got error: " + ex.Message);
                 return false;
             }
             return true;
@@ -99,17 +53,22 @@ namespace GoogleCRBot
         private async Task LoginTroughUser()
         {
             (string username, string password) = GetCreds();
+
+            // send username
             wait.Until(driver =>
                 driver.FindElement(By.CssSelector("#identifierId"))
             ).SendKeys(username + Keys.Enter);
 
-            await WaitFor(data.Delays["login"]["afterUsername"]);
+            // Delay
+            await WaitFor((int)(browserData.Delays["login"]["afterUsername"] * 1000));
 
+            // send password
             wait.Until(driver =>
                 driver.FindElement(By.CssSelector("#password > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > input:nth-child(1)"))
             ).SendKeys(password + Keys.Enter);
 
-            await WaitFor(data.Delays["login"]["afterLogin"]);
+            // Delay
+            await WaitFor((int)(browserData.Delays["login"]["afterLogin"] * 1000));
         }
         async Task WaitFor(int ms)
         {
@@ -126,8 +85,6 @@ namespace GoogleCRBot
         public void Dispose()
         {
             driver.Dispose();
-            logger.Close();
-            logger.Dispose();
         }
     }
 }
--- Data.cs
+++ Data.cs
@@ -1,13 +1,17 @@
 using System.Collections.Generic;
 
-namespace GoogleCRBot
+namespace GoogleCRBot.Data
 {
-    public class Data
+    public class GlobalData
     {
         public string ClassroomLink { get; set; }
-        public string RemoteURI { get; set; }
+        public string PreferredBrowser { get; set; }
         public string DriverFolder { get; set; }
-        public string LogFolder { get; set; }
-        public Dictionary<string, Dictionary<string, int>> Delays;
+        public BrowserData Chrome { get; set; }
+        public BrowserData Firefox { get; set; }
+    }
+    public class BrowserData
+    {
+        public Dictionary<string, Dictionary<string, float>> Delays;
     }
 }
